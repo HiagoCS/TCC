4048bd0a56d3ace284c2b6ac3339ebcf247d596e
HiagoCS <hiago8cs@gmail.com>
2022-02-19 13:13:44 -0300

    Add colunm CEP in tb_user

--- SQL/db_tcc.sql
+++ SQL/db_tcc.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS tb_user(
     tel VARCHAR(13) NOT NULL,
     password VARCHAR(32) NOT NULL,
     status VARCHAR(32) NOT NULL,
+    cep int(10) NOT NULL,
     category_id INT,
     level INT NOT NULL,
     FOREIGN KEY (category_id) REFERENCES tb_category(id),
